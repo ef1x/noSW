31a849a3460a477bf738bb1ec9cdaa9a53253475
User team054 <team054@local.host>
2015-03-03 16:02:15 +0100

    sw active, with content

--- templates/persons/personsController.js
+++ templates/persons/personsController.js
@@ -2,15 +2,15 @@ angular.module('swApp.controllers', [])
     .controller('personsCtrl', ['$rootScope', '$scope', 'Person', '$log',
         function ($rootScope, $scope, Person, $log) {
 
-            //$scope.getPerson = function () {
-            //
-            //    Person.query(function (persons) {
-            //            $scope.personsArray = persons;
-            //            $rootScope.$broadcast('scroll.refreshComplete');
-            //        });
-            //};
-            //
-            //$scope.getPerson()
+            $scope.getPerson = function () {
+
+                Person.query(function (persons) {
+                        $scope.personsArray = persons;
+                        $rootScope.$broadcast('scroll.refreshComplete');
+                    });
+            };
+
+            $scope.getPerson()
         }
     ])
 ;
